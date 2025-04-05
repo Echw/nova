a3d2ca004d1ea699a08a8424de1a925d5985cbc2
echw <echwedczuk0@gmail.com>
2025-04-05 16:13:10 +0200

    add minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
+import "./typography.scss";
 import { Provider } from "@/components/ui/provider";
 
 const geistSans = Geist({
